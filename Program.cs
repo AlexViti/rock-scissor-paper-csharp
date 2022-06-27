e1b786d26c318624de1406e97f46c1079227279b
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-27 16:59:57 +0200

    Minor tweaks

--- Program.cs
+++ Program.cs
@@ -4,16 +4,14 @@
  * if user wins, user gets 1 point
  * if cpu wins, cpu gets 1 point
  * if tie, nobody gets point
- * if user gets 3 points, user wins
- * if cpu gets 3 points, cpu wins
- * best to N input by player, wins
+ * best to N input by player
 */
 char[] choices = { 'r', 's', 'p' };
 string[] choiceNames = { "Rock", "Scissor", "Paper" };
 
 Console.WriteLine("Rock, Scissor, Paper");
 
-Console.WriteLine("Choose an odd number as the number of games you want to play");
+Console.WriteLine("Choose an odd number for the number of games you want to play");
 
 string? maxGamesInput = Console.ReadLine();
 while (!int.TryParse(maxGamesInput, out _) || int.Parse(maxGamesInput) % 2 == 0)
@@ -35,14 +33,14 @@ while (userScore <= maxGames / 2 && cpuScore <= maxGames / 2)
 
     while (Array.IndexOf(choices, userChoiceChar) == -1)
     {
-        Console.WriteLine("Choose a valid choice");
+        Console.WriteLine("Press a valid key! ('r', 's' or 'p')");
         userChoiceChar = Console.ReadKey().KeyChar;
         Console.WriteLine();
     }
 
     int userChoice = Array.IndexOf(choices, userChoiceChar);
-
     int cpuChoice = new Random().Next(0, 3);
+    
     Console.WriteLine();
     Console.WriteLine($"CPU chose {choiceNames[cpuChoice]}");
     Console.WriteLine($"You chose {choiceNames[userChoice]}");
