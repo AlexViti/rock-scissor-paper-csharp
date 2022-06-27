9b03aaa2b4fe99ca640f0f1fe67b1101d60de156
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-27 17:29:52 +0200

    Add recap

--- Program.cs
+++ Program.cs
@@ -11,36 +11,45 @@ string[] choiceNames = { "Rock", "Scissor", "Paper" };
 
 Console.WriteLine("Rock, Scissor, Paper");
 
-Console.WriteLine("Choose an odd number for the number of games you want to play");
+Console.Write("Choose an odd number for the number of games you want to play: ");
 
 string? maxGamesInput = Console.ReadLine();
 while (!int.TryParse(maxGamesInput, out _) || int.Parse(maxGamesInput) % 2 == 0)
 {
-    Console.WriteLine("Choose an odd number");
+    Console.Write("Choose an odd number: ");
     maxGamesInput = Console.ReadLine();
 }
 int maxGames = int.Parse(maxGamesInput);
+int game = 1;
 
 int userScore = 0;
 int cpuScore = 0;
 
+int[] userChoices = Array.Empty<int>();
+int[] cpuChoices = Array.Empty<int>();
+
+Console.WriteLine("***********\n");
+
 while (userScore <= maxGames / 2 && cpuScore <= maxGames / 2)
 {
-    Console.WriteLine("Type 'r' for rock, 's' for scissor, 'p' for paper");
+    Console.WriteLine($"Game {game} out of {maxGames}\n");
+    Console.Write("Type 'r' for rock, 's' for scissor, 'p' for paper: ");
     char userChoiceChar = Console.ReadKey().KeyChar;
 
     Console.WriteLine();
 
     while (Array.IndexOf(choices, userChoiceChar) == -1)
     {
-        Console.WriteLine("Press a valid key! ('r', 's' or 'p')");
+        Console.Write("Press a valid key! ('r', 's' or 'p'): ");
         userChoiceChar = Console.ReadKey().KeyChar;
         Console.WriteLine();
     }
 
     int userChoice = Array.IndexOf(choices, userChoiceChar);
+    userChoices = userChoices.Append(userChoice).ToArray();
     int cpuChoice = new Random().Next(0, 3);
-    
+    cpuChoices = cpuChoices.Append(cpuChoice).ToArray();
+
     Console.WriteLine();
     Console.WriteLine($"CPU chose {choiceNames[cpuChoice]}");
     Console.WriteLine($"You chose {choiceNames[userChoice]}");
@@ -52,10 +61,12 @@ while (userScore <= maxGames / 2 && cpuScore <= maxGames / 2)
     else if ((userChoice == 0 && cpuChoice == 1) || (userChoice == 1 && cpuChoice == 2) || (userChoice == 2 && cpuChoice == 0))
     {
         Console.WriteLine("You win\nResult: You " + (++userScore) + " - CPU " + (cpuScore));
+        game++;
     }
     else
     {
         Console.WriteLine("CPU wins\nResult: You " + (userScore) + " - CPU " + (++cpuScore));
+        game++;
     }
 
     Console.WriteLine("---------\n");
@@ -69,4 +80,23 @@ if (userScore > cpuScore)
 else
 {
     Console.WriteLine("Too Bad! YOU LOST");
+}
+
+Console.WriteLine("----- RECAP -----\n");
+
+for (int i = 0; i < userChoices.Length; i++)
+{
+    Console.Write($"You chose {choiceNames[userChoices[i]]} - CPU chose {choiceNames[cpuChoices[i]]}");
+    if (userChoices[i] == cpuChoices[i])
+    {
+        Console.WriteLine(" - Tie");
+    }
+    else if ((userChoices[i] == 0 && cpuChoices[i] == 1) || (userChoices[i] == 1 && cpuChoices[i] == 2) || (userChoices[i] == 2 && cpuChoices[i] == 0))
+    {
+        Console.WriteLine(" - You won");
+    }
+    else
+    {
+        Console.WriteLine(" - CPU won");
+    }
 }
