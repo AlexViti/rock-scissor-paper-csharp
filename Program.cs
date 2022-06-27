cc95830ebf88ebd763d4de904e9df112206a3c96
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-27 16:42:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿/* Rock, scissor, paper
+ * 2 players
+ * cpu and user choose between rock, scissor, paper
+ * if user wins, user gets 1 point
+ * if cpu wins, cpu gets 1 point
+ * if tie, nobody gets point
+ * if user gets 3 points, user wins
+ * if cpu gets 3 points, cpu wins
+ * best to N input by player, wins
+*/
+char[] choices = { 'r', 's', 'p' };
+string[] choiceNames = { "Rock", "Scissor", "Paper" };
+
+Console.WriteLine("Rock, Scissor, Paper");
+
+Console.WriteLine("Choose an odd number as the number of games you want to play");
+
+string? maxGamesInput = Console.ReadLine();
+while (!int.TryParse(maxGamesInput, out _) || int.Parse(maxGamesInput) % 2 == 0)
+{
+    Console.WriteLine("Choose an odd number");
+    maxGamesInput = Console.ReadLine();
+}
+int maxGames = int.Parse(maxGamesInput);
+
+int userScore = 0;
+int cpuScore = 0;
+
+while (userScore <= maxGames / 2 && cpuScore <= maxGames / 2)
+{
+    Console.WriteLine("Type 'r' for rock, 's' for scissor, 'p' for paper");
+    char userChoiceChar = Console.ReadKey().KeyChar;
+
+    Console.WriteLine();
+
+    while (Array.IndexOf(choices, userChoiceChar) == -1)
+    {
+        Console.WriteLine("Choose a valid choice");
+        userChoiceChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+    }
+
+    int userChoice = Array.IndexOf(choices, userChoiceChar);
+
+    int cpuChoice = new Random().Next(0, 3);
+    Console.WriteLine();
+    Console.WriteLine($"CPU chose {choiceNames[cpuChoice]}");
+    Console.WriteLine($"You chose {choiceNames[userChoice]}");
+
+    if (userChoice == cpuChoice)
+    {
+        Console.WriteLine("Tie");
+    }
+    else if ((userChoice == 0 && cpuChoice == 1) || (userChoice == 1 && cpuChoice == 2) || (userChoice == 2 && cpuChoice == 0))
+    {
+        Console.WriteLine("You win\nResult: You " + (++userScore) + " - CPU " + (cpuScore));
+    }
+    else
+    {
+        Console.WriteLine("CPU wins\nResult: You " + (userScore) + " - CPU " + (++cpuScore));
+    }
+
+    Console.WriteLine("---------\n");
+}
